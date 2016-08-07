249ccc589c2fdf7b1a26ca081f9f3d29f3640551
Ryo <eeee.jmk@gmail.com>
2016-08-07 13:43:11 +0900

    Project Create and Carthage Installed

---
+++ RxSwift+PostViewModel/RxSwift+PostViewModel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxSwift+PostViewModel
+//
+//  Created by 福田涼介 on 8/7/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RxSwift+PostViewModel/RxSwift+PostViewModelTests/RxSwift_PostViewModelTests.swift
@@ -0,0 +1,36 @@
+//
+//  RxSwift_PostViewModelTests.swift
+//  RxSwift+PostViewModelTests
+//
+//  Created by 福田涼介 on 8/7/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import XCTest
+@testable import RxSwift_PostViewModel
+
+class RxSwift_PostViewModelTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
