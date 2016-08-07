b0e4853c28cdbc675e6dcf3c6b3aa25e75abb388
Ryo <eeee.jmk@gmail.com>
2016-08-08 00:02:33 +0900

    rx_responseFromを実装

--- RxSwift+PostViewModel/RxSwift+PostViewModel/Session+Rx.swift
+++ RxSwift+PostViewModel/RxSwift+PostViewModel/Session+Rx.swift
@@ -6,4 +6,24 @@
 //  Copyright © 2016 yuzushioh. All rights reserved.
 //
 
-import Foundation
+import RxSwift
+import APIKit
+
+extension Session {
+    func rx_responseFrom<T: RequestType>(request: T) -> Observable<T.Response> {
+        return Observable.create { observer in
+            self.sendRequest(request) { result in
+                switch result {
+                case .Success(let response):
+                    observer.onNext(response)
+                    observer.onCompleted()
+                    
+                case .Failure(let error):
+                    observer.onError(error)
+                }
+            }
+            
+            return AnonymousDisposable {}
+        }
+    }
+}
