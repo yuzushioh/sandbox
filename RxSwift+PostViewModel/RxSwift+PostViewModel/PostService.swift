08babdc4940cee6c6eb43fc9e5f3ee6e313ba702
Ryo <eeee.jmk@gmail.com>
2016-08-08 00:01:35 +0900

    必要なファイルを作成

---
+++ RxSwift+PostViewModel/RxSwift+PostViewModel/PostService.swift
@@ -0,0 +1,9 @@
+//
+//  PostService.swift
+//  RxSwift+PostViewModel
+//
+//  Created by 福田涼介 on 8/7/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import Foundation
---
+++ RxSwift+PostViewModel/RxSwift+PostViewModel/PostViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxSwift+PostViewModel
+//
+//  Created by 福田涼介 on 8/7/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import UIKit
+
+class PostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
