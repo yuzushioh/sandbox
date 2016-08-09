46f99a9a2f5cf9ef8ca0273a9ffc9844c94f2c9c
Ryo <eeee.jmk@gmail.com>
2016-08-10 00:15:54 +0900

    CategoryListViewControllerを生成

---
+++ RxSwift+PostViewModel/RxSwift+PostViewModel/CategoryListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryListViewController.swift
+//  RxSwift+PostViewModel
+//
+//  Created by 福田涼介 on 8/10/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class CategoryListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
---
+++ RxSwift+PostViewModel/RxSwift+PostViewModel/CategoryListViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CategoryListViewModel.swift
+//  RxSwift+PostViewModel
+//
+//  Created by 福田涼介 on 8/10/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class CategoryListViewModel {
+    
+    init() {
+        
+    }
+}
