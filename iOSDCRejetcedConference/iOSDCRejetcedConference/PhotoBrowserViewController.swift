b7b761ea93629bcfb6634a5dee7710ab17ee73d0
Ryo <eeee.jmk@gmail.com>
2016-08-13 23:29:35 +0900

    画像とファイルを作成

---
+++ iOSDCRejetcedConference/iOSDCRejetcedConference/PhotoBrowserViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoBrowserViewController.swift
+//  iOSDCRejetcedConference
+//
+//  Created by 福田涼介 on 8/13/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import Foundation
---
+++ iOSDCRejetcedConference/iOSDCRejetcedConference/PhotoViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoViewCell.swift
+//  iOSDCRejetcedConference
+//
+//  Created by 福田涼介 on 8/13/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewCell: UICollectionViewCell {
+    
+}
