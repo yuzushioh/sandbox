d4aec36e3ed1b19ca6b9e9abef82ba61480580cc
Ryo <eeee.jmk@gmail.com>
2016-08-13 15:28:31 +0900

    sandboxにiOSDC Rejected Conferenceのプロジェクトを追加

---
+++ iOSDCRejetcedConference/iOSDCRejetcedConference/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSDCRejetcedConference
+//
+//  Created by 福田涼介 on 8/13/16.
+//  Copyright © 2016 yuzushioh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
