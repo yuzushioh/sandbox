475c419582ca2601429b25b391c1da956aa1c2db
Ryo <eeee.jmk@gmail.com>
2016-08-13 23:30:08 +0900

    structでPhotoを定義

--- iOSDCRejetcedConference/iOSDCRejetcedConference/Photo.swift
+++ iOSDCRejetcedConference/iOSDCRejetcedConference/Photo.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+struct Photo {
+    let photoName: String
+}
+
+extension Photo {
+    static var photos: [Photo] {
+        return [
+            Photo(photoName: "image1"),
+            Photo(photoName: "image2"),
+            Photo(photoName: "image3"),
+            Photo(photoName: "image4"),
+            Photo(photoName: "image5"),
+            Photo(photoName: "image6"),
+            Photo(photoName: "image7"),
+            Photo(photoName: "image8"),
+            Photo(photoName: "image9"),
+            Photo(photoName: "image10"),
+            Photo(photoName: "image11"),
+            Photo(photoName: "image12"),
+            Photo(photoName: "image1"),
+            Photo(photoName: "image2"),
+            Photo(photoName: "image3"),
+            Photo(photoName: "image4"),
+            Photo(photoName: "image5"),
+            Photo(photoName: "image6"),
+            Photo(photoName: "image7"),
+            Photo(photoName: "image8"),
+            Photo(photoName: "image9"),
+            Photo(photoName: "image10"),
+            Photo(photoName: "image11"),
+            Photo(photoName: "image12")
+        ]
+    }
+}
